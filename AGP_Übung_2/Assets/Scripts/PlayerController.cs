c21e127e573c36455e60670accfc619d8c87a8ad
SVorn <stephan@vornefeld.net>
2018-06-15 12:21:03 +0200

    Added Player Movement and changing directions

--- AGP_Übung_2/Assets/Scripts/PlayerController.cs
+++ AGP_Übung_2/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	
+	private Rigidbody2D rigidbodyPlayer;
+
+	[SerializeField]
+	private float speed;
+
+	private bool facingRightDirection;
 	// Use this for initialization
 	void Start () {
-		
+		rigidbodyPlayer = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
+	//FixedUpdate when dealing with different fps
 	void Update () {
-		
+		float horizontal = Input.GetAxis("Horizontal"); //control via InputManager -> multiple control possibilities
+		//horizontal values from -1 to 1
+		//Debug.Log(horizontal);
+		Movement(horizontal);
+		changeDirection(horizontal);
 	}
+
+	private void Movement(float horizontal){
+	//	rigidbodyPlayer.velocity = Vector2.left; //.left = x = -1, y = 0;
+		rigidbodyPlayer.velocity = new Vector2(horizontal * speed, rigidbodyPlayer.velocity.y);
+	}
+
+	private void changeDirection(float horizontal){
+		if(horizontal < 0 && !facingRightDirection || horizontal > 0 && facingRightDirection){
+			facingRightDirection = !facingRightDirection;
+
+			Vector3 playerScale = transform.localScale;
+
+			//only works when scale x set to -1 at start?? -> solved its the if-condition
+			playerScale.x *= -1;
+
+			transform.localScale = playerScale;
+		}
+	}
+
 }
