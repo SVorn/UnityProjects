2eccbf186fc0b589086ea601a8c3b4b18e718603
Stephan Vornefeld <stephan@vornefeld.net>
2018-07-16 10:07:13 +0200

    Added Invader Spawner

---
+++ AGP_Übung_3/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject invader;
+
+	[SerializeField]
+	private float spawnDistance;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(SpawnInvader());
+	}
+	
+	//Method to Spawn the Invaders
+
+	IEnumerator SpawnInvader(){
+		Vector3 pos = Random.onUnitSphere * 20f;
+		Instantiate(invader, pos, Quaternion.identity);
+
+		yield return new WaitForSeconds(5f);
+
+		StartCoroutine(SpawnInvader());
+	}
+}
