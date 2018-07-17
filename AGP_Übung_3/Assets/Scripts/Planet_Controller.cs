b5fe1b777c512fb3c10eac73cb28003623a7f69f
Stephan Vornefeld <stephan@vornefeld.net>
2018-07-17 21:54:27 +0200

    Merge Repair...

--- AGP_Übung_3/Assets/Scripts/Planet_Controller.cs
+++ AGP_Übung_3/Assets/Scripts/Planet_Controller.cs
@@ -18,17 +18,11 @@ public class Planet_Controller : MonoBehaviour {
 
 	private float currentTime;
 
-<<<<<<< HEAD
-	float timeToMove = 5f;
-
-	private Vector3 destinationScale;
-=======
 
 	private Vector3 destinationScale;
 
 	[SerializeField]
 	private float scaleSpeed;
->>>>>>> branch
 
 	private float value = 0f;
 
@@ -39,18 +33,10 @@ public class Planet_Controller : MonoBehaviour {
 	private GameController gameController;
 	
 	[SerializeField]
-<<<<<<< HEAD
-	private float scaleSpeed;
-
-	private float value = 0f;
-
-	private float speed;
-=======
 	private GameObject destroyEffect;
 
 	Vector3 vec3 = new Vector3(0,0,0);
 
->>>>>>> branch
 	// Use this for initialization
 	void Start () {
 		amountOfHits = 0;
@@ -60,38 +46,6 @@ public class Planet_Controller : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update(){
-<<<<<<< HEAD
-	/*	if(currentTime <= timeToMove){
-			currentTime += Time.deltaTime;
-		}
-		value = 0;*/
-
-	//	destinationScale = new Vector3(transform.localScale.x + value, transform.localScale.y + value, transform.localScale.z + value);
-/*		float i = 0f;
-		while(i < 1.0){
-			i += Time.deltaTime * scaleSpeed;
-			transform.localScale = Vector3.Lerp(transform.localScale, destinationScale, i);
-		
-		}
-		value = 0;*/
-	}
-	
-	// Update is called once per frame
-/*	void LateUpdate () {
-	//	destinationScale = new Vector3(transform.localScale.x + value, transform.localScale.y + value, transform.localScale.z + value);
-	//	transform.localScale = Vector3.MoveTowards(transform.localScale, destinationScale, scaleSpeed*Time.deltaTime);
-
-	//Old Planet Gravity, allows for cool fly in moments!!!
-
-		Collider[] colliders = Physics.OverlapSphere(transform.position, pullRadius, layersToPull);
-
-		foreach(var collider in colliders){
-			Rigidbody rb = collider.GetComponent<Rigidbody>();
-
-			if(rb == null) continue;
-
-			Vector3 direction = transform.position - collider.transform.position;
-=======
 		destinationScale = new Vector3(transform.localScale.x + value, transform.localScale.y + value, transform.localScale.z + value);
 		transform.localScale = Vector3.Lerp(transform.localScale, destinationScale, scaleSpeed*Time.deltaTime);
 	
@@ -106,7 +60,6 @@ public class Planet_Controller : MonoBehaviour {
 			gameController.LifeBar();
 		}
 	}
->>>>>>> branch
 
 
 	public void Increment(float increment){
@@ -115,33 +68,9 @@ public class Planet_Controller : MonoBehaviour {
 	//	destinationScale = new Vector3(transform.localScale.x + value, transform.localScale.y + value, transform.localScale.z + value);
 	}
 
-<<<<<<< HEAD
-			rb.AddForce(direction.normalized * (gravityPull/distance)* rb.mass * Time.deltaTime);
-			}
-
-	}*/
-
-	public void Increment(float increment){
-		Debug.Log("Increment");
-		value = value + increment;
-		Scale();
-	//	destinationScale = new Vector3(transform.localScale.x + value, transform.localScale.y + value, transform.localScale.z + value);
-	}
-
-	void Scale(){
-		Debug.Log("Scale");
-		destinationScale = new Vector3(transform.localScale.x + value, transform.localScale.y + value, transform.localScale.z + value);
-		float i = 0f;
-		while(i < 1.0){
-			i += Time.deltaTime * scaleSpeed;
-			transform.localScale = Vector3.Lerp(transform.localScale, destinationScale, i);
-		
-		}
-		value = 0;
-=======
 	//Planet can be hit to 3 times, after that it "destroyed" -> no actual destroy because it is an asset
 	void TakesHit(){
-		if(amountOfHits >= 2){
+		if(amountOfHits >= 3){
 			Debug.Log("End Game");
 			Instantiate(destroyEffect, vec3 ,transform.rotation);
 			gameController.GameOver();
@@ -150,7 +79,6 @@ public class Planet_Controller : MonoBehaviour {
 		amountOfHits += 1;
 		Increment(1);
 		Debug.Log(amountOfHits);
->>>>>>> branch
 	}
 }
 
--- AGP_Übung_3/Assets/Scripts/Player_Controller.cs
+++ AGP_Übung_3/Assets/Scripts/Player_Controller.cs
@@ -13,15 +13,12 @@ public class Player_Controller : MonoBehaviour {
 	private Rigidbody rigidPlayer;
 
 	Vector3 movement;
-<<<<<<< HEAD
-=======
 
 	[SerializeField]
 	private GameObject defensePrefab;
 
 	private float amountOfTowers;
 
->>>>>>> branch
 	// Use this for initialization
 	void Start () {
 		amountOfTowers = 0;
@@ -30,38 +27,18 @@ public class Player_Controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-<<<<<<< HEAD
-	//	horizontal = Input.GetAxis("Horizontal")* speed * Time.deltaTime;
-	//  vertical = Input.GetAxis("Vertical")* speed * Time.deltaTime;
-	//transform.Rotate(Vector3.forward*Input.GetAxis("Horizontal")*Time.deltaTime *speed);
-	
-		
-	//Rotate so Player is always moving forward
-/*	if(Input.GetKey(KeyCode.A)){
-		transform.Rotate(Vector3.forward*1*Time.deltaTime*turnSpeed);
-	}
-	if(Input.GetKey(KeyCode.D)){
-		transform.Rotate(Vector3.forward*-1*Time.deltaTime*turnSpeed);
-	}
-*/
-	Vector3 targetDir = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0f).normalized;
-	movement = targetDir*speed;
-=======
-//calculate direction to move from -1 to 1
+
+	//calculate direction to move from -1 to 1
 	Vector3 targetDir = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0f).normalized;
 	movement = targetDir*speed;
 
-//Instantiate defense system on player position by pressing space
-//	if(amountOfTowers < 3){
-		Vector3 pos = new Vector3(transform.position.x,transform.position.y,transform.position.z);
-		if(Input.GetKeyDown(KeyCode.Space)){
-			Instantiate(defensePrefab, pos, transform.rotation);
-			amountOfTowers += 1;
-			}
-//		} else{
+	//Instantiate defense system on player position by pressing space
+	Vector3 pos = new Vector3(transform.position.x,transform.position.y,transform.position.z);
+	if(Input.GetKeyDown(KeyCode.Space)){
+		Instantiate(defensePrefab, pos, transform.rotation);
+		amountOfTowers += 1;
+		}
 		
->>>>>>> branch
 	}
 	
 	
@@ -74,57 +51,3 @@ public class Player_Controller : MonoBehaviour {
 	}
 
 }
-	//	horizontal = Input.GetAxis("Horizontal")* speed * Time.deltaTime;
-	//  vertical = Input.GetAxis("Vertical")* speed * Time.deltaTime;
-	//transform.Rotate(Vector3.forward*Input.GetAxis("Horizontal")*Time.deltaTime *speed);
-	
-		
-	//Rotate so Player is always moving forward
-/*	if(Input.GetKey(KeyCode.A)){
-		transform.Rotate(Vector3.forward*1*Time.deltaTime*turnSpeed);
-	}
-	if(Input.GetKey(KeyCode.D)){
-		transform.Rotate(Vector3.forward*-1*Time.deltaTime*turnSpeed);
-	}
-*/
-/*	void Movement(float vertical, float horizontal){
-		rigidPlayer.velocity = new Vector3(rigidPlayer.velocity.x,vertical * speed, rigidPlayer.velocity.z);
-		//transform.position = new Vector3(vertical * speed, 0f, 0f);
-
-		rigidPlayer.velocity = new Vector3(horizontal * speed, rigidPlayer.velocity.y,0f);
-	}*/
-
-/*	void Rotate(Vector3 origin, Vector3 axis, float angle){
-		Quaternion q = Quaternion.AngleAxis(angle, axis);
-		rigidPlayer.MovePosition(q * (rigidPlayer.transform.position - origin)+origin);
-		rigidPlayer.MoveRotation(rigidPlayer.transform.rotation*q);
-	}*/
-
-<<<<<<< HEAD
-	void FixedUpdate(){
-		rigidPlayer.MovePosition(rigidPlayer.position + transform.TransformDirection(movement)* Time.fixedDeltaTime);
-	//	rigidPlayer.MoveRotation(rigidPlayer.transform.rotation);
-		//Original Code
-=======
-	//	rigidPlayer.MoveRotation(rigidPlayer.transform.rotation);
-		
->>>>>>> branch
-/*		Vector3 origin = Vector3.zero;
-
-		Quaternion hori = Quaternion.AngleAxis(-horizontal, Vector3.up);
-		Quaternion vert = Quaternion.AngleAxis(vertical, Vector3.right);
-
-		Quaternion q = hori * vert;
-		rigidPlayer.MovePosition(q*(rigidPlayer.transform.position-origin)+origin);
-*/		
-
-		//rigidPlayer.MoveRotation(rigidPlayer.transform.rotation*q);
-		//transform.LookAt(Vector3.zero);
-<<<<<<< HEAD
-
-	}
-
-}
-
-=======
->>>>>>> branch
